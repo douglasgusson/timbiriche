f6099f8a8d50ef0fafd92e82d1fae409aad86d14
Douglas Gusson <douglasgusson@gmail.com>
2022-02-26 12:22:09 -0300

    👍 Extração do componente de jogado atual

---
+++ src/components/CurrentPlayer.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { storeToRefs } from "pinia";
+import { useGameStore } from "@/stores/game";
+
+const game = useGameStore();
+const { currentPlayer } = storeToRefs(game);
+</script>
+
+<template>
+  <div>
+    É a vez do
+    <strong class="player-name">{{ currentPlayer.name }}</strong>
+  </div>
+</template>
+
+<style scoped>
+.player-name {
+  padding: 0.2rem;
+  font-weight: 800;
+  border-bottom: 2px solid v-bind(currentPlayer.color);
+  transition: 0.3s;
+  will-change: border-bottom;
+}
+</style>
--- src/views/Timbiriche.vue
+++ src/views/Timbiriche.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Board from "@/components/Board.vue";
 import Scoreboard from "../components/Scoreboard.vue";
+import CurrentPlayer from "../components/CurrentPlayer.vue";
 </script>
 
 <template>
   <div class="timbiriche">
     <Scoreboard />
+    <CurrentPlayer />
     <Board :rows="6" :columns="6" />
   </div>
 </template>
